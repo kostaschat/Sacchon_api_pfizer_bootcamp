00b8f5f22d81d4d7152bd049477da08f5362cd2f
kostaschat <kostaschatziathanasiou@hotmail.com>
2020-09-16 10:16:22 -0700

    mediDatarepo

--- src/main/java/com/pfizer/sacchonapi/security/dao/DatabaseCredentials.java
+++ src/main/java/com/pfizer/sacchonapi/security/dao/DatabaseCredentials.java
@@ -2,7 +2,7 @@ package com.pfizer.sacchonapi.security.dao;
 
 public class DatabaseCredentials {
 
-    public static final String URL = "jdbc:sqlserver://localhost;databaseName=sacchon;";
+    public static final String URL = "jdbc:sqlserver://localhost;databaseName=codehub;";
     public static final String USER = "sa";
     public static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     public static final String PASSWORD = "admin!@#123";
