2c1eb985904111231fb95f3d845539ee49830a32
anestis almaliotis <anestis-almal@hotmail.com>
2020-10-02 01:14:01 +0300

    Angular last branch for merge

--- src/main/java/com/pfizer/sacchonapi/exception/BadEntityException.java
+++
@@ -1,7 +0,0 @@
-package com.pfizer.sacchonapi.exception;
-
-public class BadEntityException extends Exception {
-    public BadEntityException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/pfizer/sacchonapi/resource/constant/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.pfizer.sacchonapi.resource.constant;
-
-public class Constants {
-    public static final String SQL_STATE_23000_DUPLICATE = "23000";
-}
